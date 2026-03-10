e2256cb01ae0a62987e71b10e10960a70d2c44cf
Ryan Henrique <ryan.henrique.3226@gmail.com>
2026-03-10 20:43:11 -0300

    feat: melhorias no controller

--- src/main/java/com/ryan/taskmanager/controller/TaskController.java
+++ src/main/java/com/ryan/taskmanager/controller/TaskController.java
@@ -1,6 +1,9 @@
 package com.ryan.taskmanager.controller;
 
 import com.ryan.taskmanager.model.Task;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.ryan.taskmanager.service.TaskService;
 
@@ -22,23 +25,27 @@ public class TaskController {
     }
 
     @PostMapping
-    public Task createTask(@RequestBody Task task){
-        return service.createTask(task);
+    public ResponseEntity<Task> createTask(@RequestBody Task task){
+        Task create = service.createTask(task);
+        return ResponseEntity.status(HttpStatus.CREATED).body(create);
     }
 
     @GetMapping("/{id}")
-    public Task getTaskById(@PathVariable int id){
-        return service.findTaskById(id);
+    public ResponseEntity<Task> getTaskById(@PathVariable int id){
+        Task task = service.findTaskById(id);
+        return ResponseEntity.ok(task);
     }
 
     @PutMapping("/{id}")
-    public Task updateTaskById(@PathVariable int id, @RequestBody Task task){
-        return service.updateById(id, task);
+    public ResponseEntity<Task> updateTaskById(@PathVariable int id, @RequestBody Task task){
+        Task update = service.updateById(id, task);
+        return ResponseEntity.ok(update);
     }
 
     @PutMapping("/{id}/status")
-    public Task updateStatus(@PathVariable int id, @RequestBody Task task){
-        return service.updateStatus(id, task.getStatus());
+    public ResponseEntity<Task> updateStatus(@PathVariable int id, @RequestBody Task task){
+        Task update = service.updateStatus(id, task.getStatus());
+        return ResponseEntity.ok(update);
     }
 
     @DeleteMapping("/{id}")
